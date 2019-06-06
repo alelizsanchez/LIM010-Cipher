bb8293c607ebcd2c77957720fcef72db4e5642ee
Alelizsanchez <Alelizsanchez@gmail.com>
2019-06-06 10:38:50 -0500

    Cifrado ale

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 /* Acá va tu código */
+const boton_entrar = document.getElementById("boton_entrar");
+let incorrectos = 0;
+
+boton_entrar.addEventListener("click", () => {
+  // console.log(document.getElementById("pass").value);
+
+  if ("laboratoria" === document.getElementById("pass").value) {
+
+    if (incorrectos < 3) {
+      console.log("Entraste a tu cuenta");
+    }
+
+  } else {
+    console.log("incorrecto");
+    incorrectos = incorrectos + 1;
+
+  }
+});
+/* Acá va tu código */
