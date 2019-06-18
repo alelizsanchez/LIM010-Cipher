9c5e202518ef8be8df6d08f169ed3fb8891b5e58
Alelizsanchez <Alelizsanchez@gmail.com>
2019-06-18 13:19:18 -0500

    test terminado

--- src/cipher.js
+++ src/cipher.js
@@ -5,7 +5,7 @@ window.cipher = {
     for(let i=0; i<string.length; i++){ /*contador*/
       let asciinum= string.charCodeAt(i); /*indica la posicion de mis letras*/
      if (65 <= asciinum && asciinum <= 90)  /*la cantidad que empieza mayusculas 65 al 90*/
-        resultado += String.fromCharCode((asciinum - 65 + offset) % 26 + 65);  /*formula de michel*/
+        resultado += String.fromCharCode((asciinum - 65 + offset) % 26 + 65);  
      else if (97 <= asciinum && asciinum <= 122) /*la cantidad que empiza la minuscula 97 al 122*/
         resultado += String.fromCharCode((asciinum - 97 + offset) % 26 + 97);
      else if(asciinum === 32 ) /* el numero de spam*/
